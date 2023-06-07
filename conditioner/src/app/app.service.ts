e6ad333d1227b66651bae937ca2984f2d2827405
alexMercy <68870653+alexMercy@users.noreply.github.com>
2023-06-07 15:30:02 +0300

    add changeLatest method

--- conditioner/src/app/app.service.ts
+++ conditioner/src/app/app.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, combineLatest} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,36 +9,33 @@ export class AppService {
   private _powerSocket = new BehaviorSubject(false);
   public powerSocket$ = this._powerSocket.asObservable()
   togglePowerSocket() {
-    this.changeStatusConditioner();
     this._powerSocket.next(!this._powerSocket.value);
   }
 
   private _window = new BehaviorSubject(false);
   public window$ = this._window.asObservable()
   toggleWindow() {
-    this.changeStatusConditioner();
     this._window.next(!this._window.value);
   }
 
   private _controller = new BehaviorSubject(false);
   public controller$ = this._controller.asObservable()
   toggleController() {
-    this.changeStatusConditioner();
     this._controller.next(!this._controller.value)
   }
 
   private _temperature = new BehaviorSubject(false);
   public temperature$ = this._temperature.asObservable()
   toggleTemperature() {
-    this.changeStatusConditioner();
     this._temperature.next(!this._temperature.value)
   }
 
-  _conditioner = new BehaviorSubject(false);
-  conditioner$ = this._conditioner.asObservable();
-  public changeStatusConditioner() {
-    this._conditioner.next(this._controller.value && this._powerSocket.value && this._temperature.value && this._window.value);
-  }
+
+  conditioner$ = combineLatest(
+    [this.powerSocket$, this.window$, this.controller$, this.temperature$],
+    (powerSocket, window,controller, temperature) =>
+      powerSocket && window && controller && temperature)
+
   constructor() { }
 
 
