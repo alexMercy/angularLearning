41b8b1386a56a21030dda0173e60aec377a169bc
alexMercy <68870653+alexMercy@users.noreply.github.com>
2023-06-08 10:30:55 +0300

    draft todo

--- todo-app/src/app/component/column/column.component.ts
+++ todo-app/src/app/component/column/column.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {CdkDragDrop, transferArrayItem, moveItemInArray} from '@angular/cdk/drag-drop';
 import {Task} from "../task/task.component";
+import {Column} from "../columns/columns.component";
 
 
 @Component({
@@ -10,21 +11,7 @@ import {Task} from "../task/task.component";
 })
 export class ColumnComponent {
 
-  @Input() tasks:Task[] = [
-    {
-      id: '1',
-      title: 'ДЗ',
-    },
-    {
-      id: '2',
-      title: 'Практика',
-    },
-    {
-      id: '3',
-      title: 'Курсач',
-      description: "Описание"
-    },
-  ];
+  @Input() columnData!:Column
   drop(event: CdkDragDrop<Task[]>) {
     const {currentIndex, previousIndex, container, previousContainer} = event;
     event.previousContainer === event.container
--- todo-app/src/app/component/columns/columns.component.ts
+++ todo-app/src/app/component/columns/columns.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {Task} from "../task/task.component";
+
+export interface Column {
+  title: string,
+  tasks: Task[]
+}
 
 @Component({
   selector: 'app-columns',
@@ -6,5 +12,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./columns.component.css']
 })
 export class ColumnsComponent {
+  data:Column[] = [
+    {
+      title: 'Todo',
+      tasks: [
+        {
+          id: '1',
+          title: 'ДЗ',
+        },
+        {
+          id: '2',
+          title: 'Практика',
+        },
+      ]
+    },
+    {
+      title: 'Done',
+      tasks: [
+        {
+          id: '3',
+          title: 'Курсач',
+          description: "Описание"
+        },
+        {
+          id: '4',
+          title: 'Просто 4',
+          description: "Описание 4"
+        },
+      ]
+    },
 
+  ]
 }
